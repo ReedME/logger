d0c3bba331fe98b646433445bb6f35bd905770fe
Brendan Reed <brendan.james.reed@gmail.com>
2018-05-25 21:58:41 +1000

    started structuring application by adding navbar and installing aws amp

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Docket Logger</h1>
+          <p>a docket logging app</p>
+        </div>
+      </div>
+    );
+  }
+}
